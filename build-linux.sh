d94c73d0b9e73fd6ffdc683044d36aab9025d70d
Aleksey Kontsevich <ext-aleksey.kontsevich@qt.io>
2018-12-23 04:07:28 +0200

    Update Linux script

--- build-linux.sh
+++ build-linux.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# export OPENSSL_VERSION="openssl-1.0.2o"
+curl -O "https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz"
+tar xfz "${OPENSSL_VERSION}.tar.gz"
+
 OPENSSL_CONFIG_OPTIONS=$(cat config-params.txt)
 
 OUTPUT_DIR="libs/linux"
@@ -13,15 +17,14 @@ mkdir $OUTPUT_DIR
 build_linux() {
   ARCH=$1
   echo "Building linux libcrypto.a for ${ARCH}"
-  
-  cd vendor/openssl
-  git clean -dfx && git checkout -f
-  
+
+	cd "${OPENSSL_VERSION}"
+
   # Config:
   ./Configure dist
 
   if [[ $ARCH == "x86_64" || $ARCH == "amd64" ]]; then
-    ./config ${OPENSSL_CONFIG_OPTIONS} -fPIC
+    ./config ${OPENSSL_CONFIG_OPTIONS} -fPIC shared threads no-asm no-sse2
   else
     setarch i386 ./config ${OPENSSL_CONFIG_OPTIONS} -m32 -fPIC
   fi
@@ -30,19 +33,28 @@ build_linux() {
   rm -rf test
 
   # Make depend:
-  make depend
+  make depend -j8
 
   # Make libcrypto:
-  make build_crypto
+  make build_libs -j8
 
   # Copy libcrypto.a to temp output directory:
   file libcrypto.a
-  mkdir ../../${OUTPUT_DIR}/${ARCH}
-  cp libcrypto.a ../../${OUTPUT_DIR}/${ARCH}
+  file libcrypto.so
+  file libcrypto.so
+  file libssl.so
+
+  mkdir ../${OUTPUT_DIR}/${ARCH}
+
+  cp libcrypto.a ../${OUTPUT_DIR}/${ARCH}/libcrypto.a
+  cp libssl.a ../${OUTPUT_DIR}/${ARCH}/libssl.a
+  cp libcrypto.so ../${OUTPUT_DIR}/${ARCH}/libcrypto.so
+  cp libssl.so ../${OUTPUT_DIR}/${ARCH}/libssl.so
+
 
   # Cleanup:
-  git clean -dfx && git checkout -f
-  cd ../../
+  make clean
+  cd ..
 }
 
 build_linux "x86"
