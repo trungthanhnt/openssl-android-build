f1c8cf8c92e1d30ffa8f32bcbcdce48a306ee8a8
trungthanhnt <trungthanhnt@gmail.com>
2024-08-11 18:54:44 +0700

    Update build-android-clang.sh
    Build ok with 3.3.0

--- build-android-clang.sh
+++ build-android-clang.sh
@@ -2,23 +2,24 @@
 
 #set -v
 
-# export OPENSSL_VERSION="openssl-3.3.0"
-wget  "https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz"
-tar xfz "${OPENSSL_VERSION}.tar.gz"
+# export OPENSSL_VERSION="openssl-1.0.2o"
+#curl -O "https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz"
+#tar xfz "${OPENSSL_VERSION}.tar.gz"
 
 PROJECT_HOME=`pwd`
 PATH_ORG=$PATH
 OUTPUT_DIR="libs/android/clang"
 
 # Clean output:
-rm -rf $OUTPUT_DIR
-mkdir $OUTPUT_DIR
+#rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 
 build_android_clang() {
 
 	echo ""
 	echo "----- Build libcrypto & libssl.so for "$1" -----"
 	echo ""
+
 
 	ARCHITECTURE=$1
 	ARCH=$2
@@ -32,17 +33,17 @@ build_android_clang() {
 	#$ANDROID_NDK_HOME/build/tools/make-standalone-toolchain.sh --verbose --stl=$stl --arch=$ARCH --install-dir=$TOOLCHAIN_DIR --platform=$PLATFORM --force
 
 	# Set toolchain
-	export TOOLCHAIN_ROOT=$PROJECT_HOME/$TOOLCHAIN_DIR
-	export SYSROOT=$TOOLCHAIN_ROOT/sysroot
-	export CC=$TOOLCHAIN-clang
-	export CXX=$TOOLCHAIN-clang++
-	export AR=$TOOLCHAIN-ar
-	export AS=$TOOLCHAIN-as
-	export LD=$TOOLCHAIN-ld
-	export RANLIB=$TOOLCHAIN-ranlib
-	export NM=$TOOLCHAIN-nm
-	export STRIP=$TOOLCHAIN-strip
-	export CHOST=$TOOLCHAIN
+	#export TOOLCHAIN_ROOT=$PROJECT_HOME/$TOOLCHAIN_DIR
+	#export SYSROOT=$TOOLCHAIN_ROOT/sysroot
+	#export CC=$TOOLCHAIN-clang
+	#export CXX=$TOOLCHAIN-clang++
+	#export AR=$TOOLCHAIN-ar
+	#export AS=$TOOLCHAIN-as
+	#export LD=$TOOLCHAIN-ld
+	#export RANLIB=$TOOLCHAIN-ranlib
+	#export NM=$TOOLCHAIN-nm
+	#export STRIP=$TOOLCHAIN-strip
+	#export CHOST=$TOOLCHAIN
 	export CXXFLAGS="-std=c++11 -fPIC"
 	export CPPFLAGS="-DANDROID -fPIC"
 	#export PATH=$PATH_ORG:$TOOLCHAIN_ROOT/bin:$SYSROOT/usr/local/bin
@@ -55,7 +56,11 @@ build_android_clang() {
 	# Build openssl libraries
 	perl -pi -w -e 's/\-mandroid//g;' ./Configure
 	#./Configure $CONFIGURE_PLATFORM shared threads no-asm no-sse2
- 	./Configure $CONFIGURE_PLATFORM -D__ANDROID_API__=29
+	./Configure $CONFIGURE_PLATFORM 
+	#./Configure $CONFIGURE_PLATFORM  -D__ANDROID_API__=29 
+	#-D__ANDROID_API__=
+	#./Configure android-arm64 -D__ANDROID_API__=29
+	make
 
     # patch SONAME
     perl -pi -e 's/SHLIB_EXT=\.so\.\$\(SHLIB_MAJOR\)\.\$\(SHLIB_MINOR\)/SHLIB_EXT=\.so/g' Makefile
@@ -80,7 +85,8 @@ build_android_clang() {
 # Build libcrypto for armeabi-v7a, x86 and arm64-v8a.
 build_android_clang "armeabi-v7a" "arm"   "android-16" "arm-linux-androideabi" "android-armv7"
 build_android_clang "x86"         "x86"   "android-16" "i686-linux-android"    "android-x86"
-build_android_clang "arm64-v8a"   "arm64" "android-21" "aarch64-linux-android" "linux-generic64 -DB_ENDIAN"
+build_android_clang "x86_64"      "x86_64"   "android-21" "x86_64-linux-android"    "android-x86_64"
+build_android_clang "arm64-v8a"   "arm64" "android-21" "aarch64-linux-android" "android-arm64"
 
 export PATH=$PATH_ORG
 
